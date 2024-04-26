5ca5227440824e4fbcd264909c63feef867fd9cc
Dave Bush <davebush@gmail.com>
2024-04-26 09:21:44 -0400

    updated sls-index
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- todo/sls-index.ts
+++ todo/sls-index.ts
@@ -13,14 +13,14 @@ const handler = app.init();
 // but we can still deploy separate lambda functions with this technique.
 // In this way we can still use build in IAM authorizers if need but and
 // take advantage of the security features and deployment benefits.
-module.exports.todoCreateHandler = async (event: Object, context: Object) => {
+module.exports.todosCreateHandler = async (event: Object, context: Object) => {
   // Add any other logic here
 
   const result = await handler(event, context);
   return result;
 }
 
-module.exports.todoGetAll = async (event: Object, context: Object) => {
+module.exports.todosGetAllHandler = async (event: Object, context: Object) => {
     // Add any other logic here
 
   const result = await handler(event, context);
